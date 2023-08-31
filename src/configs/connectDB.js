a073096333baa0fcfad0378a27e0621d4cb5cead
unknown <@tri.nguyenminh>
2023-08-31 16:34:38 +0700

    config DB connection

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -1,25 +1,26 @@
 import pgPromise from 'pg-promise';
 import express from 'express';
-import * as dotenv from 'dotenv';
-dotenv.config();
+import * as dotenv from 'dotenv'
 
+dotenv.config();
 const key = process.env.DB_CONNECT_KEY;
 const app = express();
+
 //Create Database connection
 const pgp = pgPromise({/* Initialization Options */ });
 const db = pgp(key);
 
 // Test connection
 (async () => {
-    db.connect()
-        .then((obj) => {
-            console.log('Connected to database');
-            obj.done(); // success, release connection;
-        })
-        .catch((error) => {
-            console.error('ERROR:', error.message);
-        });
-})
+    try {
+        const obj = await db.connect();
+        console.log('Connected to database');
+        obj.done(); // success, release connection;
+    } catch (error) {
+        console.error('ERROR:', error.message);
+    }
+})();
+
 
     // db.query(`select * from users`, (err, res)=>{
     //     if(!err) {
